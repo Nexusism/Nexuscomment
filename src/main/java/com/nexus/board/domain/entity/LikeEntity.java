433835f948b06820daba0546685ee1f17d55bc98
Nexusism <wanggoog91@gmail.com>
2020-12-09 23:14:12 +0900

    20201209 liketable

---
+++ src/main/java/com/nexus/board/domain/entity/LikeEntity.java
@@ -0,0 +1,42 @@
+package com.nexus.board.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "liketable")
+public class LikeEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    private Long lid;
+
+    @OneToOne
+    @JoinColumn(name = "board_id")
+    private BoardEntity bid;
+
+    @OneToOne
+    @JoinColumn(name = "code")
+    private UserInfo uid;
+
+    @Column(name = "likecheck")
+    @ColumnDefault("0")
+    private Long lcheck;
+
+
+    @Builder
+    public LikeEntity(Long lid, BoardEntity bid, UserInfo uid, Long lcheck) {
+        this.lid = lid;
+        this.bid = bid;
+        this.uid = uid;
+        this.lcheck = lcheck;
+    }
+}
